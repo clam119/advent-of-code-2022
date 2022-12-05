b2b8340738eba7a7c9a970cc7df0001226e27c9a
Clam119 <Christopher.lam119@gmail.com>
2022-12-05 18:46:19 +0000

    Completely refactored to return a single answerKey that contains both the part's answers.

--- src/02-rock-paper-scissors.ts
+++ src/02-rock-paper-scissors.ts
@@ -1,95 +1,111 @@
-const fs = require('fs/promises');
+import { fetchInput } from "./utils/fetch-input";
 
-const elvenRockPaperScissors = async () => {
+interface AnswerKey {
+    partOneAnswer: number,
+    partTwoAnswer: number
+}
+
+export async function rockPaperScissors(path: string): Promise<AnswerKey> {
     // Import in the input data & split on each new line
-    const rawGameInput = fs.readFile('./data/challenge-2.txt', 'utf-8')
-    const gameInput = await rawGameInput;
+    const gameInput = await fetchInput(path);
     const splitGameInput = gameInput.split('\r\n');
-    let playerTotalScore = 0;
+
+    const answerKey: AnswerKey = {
+        partOneAnswer: 0,
+        partTwoAnswer: 0,   
+    }
 
     // Iterate through each input to check enemy output and player input
     // A, B, C = Enemy's Rock/Paper/Scissors
     // X, Y, Z = Player's Rock/Paper/Scissors
+
+    //Part 1 - Before the actual guidebooks description is revealed
     splitGameInput.forEach((input) => {
         const opponentChoice = input[0];
         const playerChoice = input[2];
 
-        // switch(playerChoice) {
-        //     case 'X':
-        //         playerTotalScore += 1;
-        //         if (opponentChoice === 'A') {
-        //             playerTotalScore += 3;
-        //         }
-        //         if (opponentChoice === 'C') {
-        //             playerTotalScore += 6;
-        //         }
-        //         break;
-        //     case 'Y':
-        //         playerTotalScore += 2;
-        //         if (opponentChoice === 'A') {
-        //             playerTotalScore += 6;
-        //         }
-        //         if (opponentChoice === 'B') {
-        //             playerTotalScore += 3;
-        //         }
-        //         break;
-        //     case 'Z':
-        //         playerTotalScore += 3;
-        //         if (opponentChoice === 'B') {
-        //             playerTotalScore += 6;
-        //         }
-        //         if (opponentChoice === 'C') {
-        //             playerTotalScore += 3;
-        //         }
-        //         break;
-        //     default:
-        //         console.log('That choice does not exist')
-        // }
+        switch(playerChoice) {
+            case 'X':
+                answerKey.partOneAnswer += 1;
+                if (opponentChoice === 'A') {
+                    answerKey.partOneAnswer += 3;
+                }
+                if (opponentChoice === 'C') {
+                    answerKey.partOneAnswer += 6;
+                }
+                break;
+            case 'Y':
+                answerKey.partOneAnswer += 2;
+                if (opponentChoice === 'A') {
+                    answerKey.partOneAnswer += 6;
+                }
+                if (opponentChoice === 'B') {
+                    answerKey.partOneAnswer += 3;
+                }
+                break;
+            case 'Z':
+                answerKey.partOneAnswer += 3;
+                if (opponentChoice === 'B') {
+                    answerKey.partOneAnswer += 6;
+                }
+                if (opponentChoice === 'C') {
+                    answerKey.partOneAnswer += 3;
+                }
+                break;
+            default:
+                console.log('That choice does not exist')
+        }
+    })
+
+    splitGameInput.forEach((input) => {
+        const opponentChoice = input[0];
+        const playerChoice = input[2];
 
         switch (playerChoice) {
             case 'X': {
-                playerTotalScore += 0; // X is intentionally losing
+                answerKey.partTwoAnswer += 0; // X is intentionally losing
                 if (opponentChoice === 'A') { //Enemy selects Rock
-                    playerTotalScore += 3; // You pick scissors and lose
+                    answerKey.partTwoAnswer += 3; // You pick scissors and lose
                 }
                 if (opponentChoice === 'B') { //Enemy selects Paper
-                    playerTotalScore += 1; // You pick rock and lose
+                    answerKey.partTwoAnswer += 1; // You pick rock and lose
                 }
                 if (opponentChoice === 'C') { //Enemy selects Scissors
-                    playerTotalScore += 2; // You pick paper and lose
+                    answerKey.partTwoAnswer += 2; // You pick paper and lose
                 }
                 break;
             }
             case 'Y': {
-                playerTotalScore += 3; // Y is intentionally drawing
+                answerKey.partTwoAnswer += 3; // Y is intentionally drawing
                 if (opponentChoice === 'A') { //Enemy selects Rock
-                    playerTotalScore += 1; // You pick scissors and draw
+                    answerKey.partTwoAnswer += 1; // You pick scissors and draw
                 }
                 if (opponentChoice === 'B') { //Enemy selects Paper
-                    playerTotalScore += 2; // You pick rock and draw
+                    answerKey.partTwoAnswer += 2; // You pick rock and draw
                 }
                 if (opponentChoice === 'C') { //Enemy selects Scissors
-                    playerTotalScore += 3; // You pick paper and draw
+                    answerKey.partTwoAnswer += 3; // You pick paper and draw
                 }
                 break;
             }
             case 'Z': {
-                playerTotalScore += 6; // Z is intentionally winning
+                answerKey.partTwoAnswer += 6; // Z is intentionally winning
                 if (opponentChoice === 'A') { //Enemy selects Rock
-                    playerTotalScore += 2; // You pick paper and win
+                    answerKey.partTwoAnswer += 2; // You pick paper and win
                 }
                 if (opponentChoice === 'B') { //Enemy selects Paper
-                    playerTotalScore += 3; // You pick scissors and win
+                    answerKey.partTwoAnswer += 3; // You pick scissors and win
                 }
                 if (opponentChoice === 'C') { //Enemy selects Scissors
-                    playerTotalScore += 1; // You pick rock and win
+                    answerKey.partTwoAnswer += 1; // You pick rock and win
                 }
                 break;
             }
+                default:
+                    console.log('That choice does not exist')
         }
     })
-
-    console.log(playerTotalScore)
+    return answerKey;
 }
 
-elvenRockPaperScissors();
+rockPaperScissors('../../inputs/02-rock-paper-scissors.txt');
