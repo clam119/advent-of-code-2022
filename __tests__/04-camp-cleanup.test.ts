3f1babd48e40108a0f9141948efb9a853afea99a
Clam119 <Christopher.lam119@gmail.com>
2022-12-06 18:50:35 +0000

    Removed console logs from camp-cleanup test.

--- __tests__/04-camp-cleanup.test.ts
+++ __tests__/04-camp-cleanup.test.ts
@@ -1,5 +1,6 @@
 import { campCleanup, convertInputToNumArr } from "../src/04-camp-cleanup";
 const testInput = '../../test-inputs/04-camp-cleanup.txt'
+
 describe('Testing the Camp Cleanup Solution', () => {
     
     it('CampCleanup function should always return an AnswerKey object with partOne and partTwo keys', async() => {
@@ -13,21 +14,15 @@ describe('Testing the Camp Cleanup Solution', () => {
     it('Should return the value of 2 for the partOne with test input', async() => {
         const result = await campCleanup(testInput);
         const partOneAnswer = result.partOneAnswer;
-        console.log(partOneAnswer)
         expect(partOneAnswer).toBe(2);
     })
 
     it('Should return the value of 4 for the partTwo with test input', async() => {
         const result = await campCleanup(testInput);
         const partTwoAnswer = result.partTwoAnswer;
-        console.log(partTwoAnswer)
         expect(partTwoAnswer).toBe(4)
         
     })
-    // Next few tests can now go indepth with checking whats going on 
-    // Inside the code, for example it should return true 
-    // if A or B's numbers are inside of each other and add counter
-    // Break down into smaller helper functions to test individually
 })
 
 describe('Testing the convertInputToNumArr Function', () => {
